4c2dc88d52c126271e2750f1f5098712ff98db92
Gordon Watts <gordon@gordonandpaula.net>
2015-09-18 10:32:01 +0200

    enable setting up rucio
    #1

--- AtlasSSH/AtlasCommands.cs
+++ AtlasSSH/AtlasCommands.cs
@@ -30,5 +30,26 @@ namespace AtlasSSH
 
             return connection;
         }
+
+        /// <summary>
+        /// Setup Rucio. ATLAS must have been previously configured, or this will "crash".
+        /// </summary>
+        /// <param name="connection">Connection on-which we will set everything up</param>
+        /// <param name="rucioUsername">The user alias used on the grid</param>
+        /// <returns></returns>
+        public static SSHConnection setupRucio(this SSHConnection connection, string rucioUsername)
+        {
+            int hashCount = 0;
+            connection
+                .ExecuteCommand(string.Format("export RUCIO_ACCOUNT={0}", rucioUsername))
+                .ExecuteCommand("localSetupRucioClients")
+                .ExecuteCommand("hash rucio", l => hashCount++);
+
+            if (hashCount != 0)
+            {
+                throw new InvalidOperationException("Unable to setup Rucio... did you forget to setup ATLAS first?");
+            }
+            return connection;
+        }
     }
 }
--- AtlasSSHTest/AtlasCommandsTest.cs
+++ AtlasSSHTest/AtlasCommandsTest.cs
@@ -15,5 +15,27 @@ namespace AtlasSSHTest
             var s = new SSHConnection(info.Item1, info.Item2);
             s.setupATLAS();
         }
+
+        [TestMethod]
+        public void setupRucioWithATLASSetup()
+        {
+            // This contains an internal check, so we only need to watch for it to "work".
+            var info = util.GetUsernameAndPassword();
+            var s = new SSHConnection(info.Item1, info.Item2);
+            s
+                .setupATLAS()
+                .setupRucio(info.Item1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void setupRucioWithoutATLASSetup()
+        {
+            // This contains an internal check, so we only need to watch for it to "work".
+            var info = util.GetUsernameAndPassword();
+            var s = new SSHConnection(info.Item1, info.Item2);
+            s
+                .setupRucio(info.Item1);
+        }
     }
 }
