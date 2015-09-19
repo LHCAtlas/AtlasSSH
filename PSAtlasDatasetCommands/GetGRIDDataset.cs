f89ac0dad50515c2d50990f5fe3a49e2530c881d
Gordon Watts <gordon@gordonandpaula.net>
2015-09-19 13:12:16 +0200

    Scafolding for a PS command
    #5

---
+++ PSAtlasDatasetCommands/GetGRIDDataset.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSAtlasDatasetCommands
+{
+    /// <summary>
+    /// Fetch a dataset from the grid using a location profile
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "GRIDDataset")]
+    public class GetGRIDDataset : PSCmdlet
+    {
+        /// <summary>
+        /// Get/Set the name of the dataset we are being asked to fetch.
+        /// </summary>
+        [Parameter(Mandatory=true, HelpMessage="Rucio dataset name to fetch", ValueFromPipeline=true, Position=1)]
+        public string DatasetName { get; set; }
+
+        /// <summary>
+        /// Setup the context
+        /// </summary>
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        /// <summary>
+        /// Fetch a particular dataset.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            WriteObject("Hi there");
+        }
+
+        /// <summary>
+        /// Cleanup
+        /// </summary>
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+}
