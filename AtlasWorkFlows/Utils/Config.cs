9f84d0ce7769784416d641305c39dcd5252777ee
Gordon Watts <gordon@gordonandpaula.net>
2015-09-23 00:10:32 +0200

    Parser to read in a config file and generate the dic<dict>> structure.
    #15

--- AtlasWorkFlows/Utils/Config.cs
+++ AtlasWorkFlows/Utils/Config.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sprache;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -45,6 +46,69 @@ namespace AtlasWorkFlows.Utils
             r["Local"] = c;
 
             return r;
+        }
+
+        #region Parsers
+
+        private static readonly Parser<string> Comment =
+            from c in ParseCharText('#')
+            from t in RestOfLine
+            select t;
+
+        private static readonly Parser<char> EndOfText =
+            Parse.LineTerminator.Or(Comment).Return(' ');
+
+        private static readonly Parser<string> Identifier =
+            Parse.Identifier(Parse.Letter, Parse.LetterOrDigit.Or(Parse.Char('_')));
+
+        private static readonly Parser<string> RestOfLine =
+            Parse.AnyChar.Until(EndOfText).Text();
+
+        private static Parser<char> ParseCharText (char c)
+        {
+            return from wh in Parse.WhiteSpace.Many()
+                   from cp in Parse.Char(c)
+                   select cp;
+        }
+
+        private static readonly Parser<System.Tuple<string, string, string>> Line
+            = from blankLines in EndOfText.Many().Optional()
+              from configName in Identifier
+              from dot1 in ParseCharText('.')
+              from parameter in Identifier
+              from dot2 in ParseCharText('=')
+              from value in RestOfLine
+              select Tuple.Create(configName, parameter, value.Trim());
+
+        private static readonly Parser<IEnumerable<Tuple<string, string, string>>> File
+            = Line.Many();
+
+        #endregion
+
+        /// <summary>
+        /// Parse a Config text file.
+        /// </summary>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        internal static Dictionary<string, Dictionary<string, string>> ParseConfigFile(System.IO.FileInfo f)
+        {
+            using (var rd = f.OpenText())
+            {
+                var input = rd.ReadToEnd();
+                var results = File.Parse(input);
+
+                var r = new Dictionary<string, Dictionary<string, string>>();
+                foreach (var t in results)
+                {
+                    if (!r.ContainsKey(t.Item1))
+                    {
+                        r[t.Item1] = new Dictionary<string, string>();
+                    }
+                    r[t.Item1][t.Item2] = t.Item3;
+                }
+
+                return r;
+            }
         }
     }
 }
---
+++ AtlasWorkFlowsTest/Utils/ConfigTest.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtlasWorkFlows.Utils;
+using System.IO;
+
+namespace AtlasWorkFlowsTest.Utils
+{
+    [TestClass]
+    public class ConfigTest
+    {
+        [TestMethod]
+        public void SingleComment()
+        {
+            var f = WriteConfigFile("SingleComment.txt", "#hi there");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(0, r.Count);
+        }
+
+        [TestMethod]
+        public void SingleLine()
+        {
+            var f = WriteConfigFile("SingleLine.txt", "UW.Name=george");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void NameWithUnderscore()
+        {
+            var f = WriteConfigFile("SingleLine.txt", "UW.name_is_good=george");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("name_is_good"));
+            Assert.AreEqual("george", r["UW"]["name_is_good"]);
+        }
+
+        [TestMethod]
+        public void SingleLineWithSpaceAfterEquals()
+        {
+            var f = WriteConfigFile("SingleLine.txt", "UW.Name= george");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void SingleLineWithSpaceAroundDots()
+        {
+            var f = WriteConfigFile("SingleLine.txt", "UW.Name= george");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void SingleLineWithSpaceBeforeEquals()
+        {
+            var f = WriteConfigFile("SingleLine.txt", "UW.Name =george");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void SingleLineWithSpace1()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace1.txt", "UW.Name=george is here");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george is here", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void SingleLineWithSpace2()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace2.txt", "UW.Name= george is here");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george is here", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void SingleLineWithSpace3()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "UW.Name= george is here ");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george is here", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void TwoLines()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "UW.Name=george", "UW.NoWay=YES");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.IsTrue(r["UW"].ContainsKey("Name"));
+            Assert.AreEqual("george", r["UW"]["Name"]);
+            Assert.IsTrue(r["UW"].ContainsKey("NoWay"));
+            Assert.AreEqual("YES", r["UW"]["NoWay"]);
+        }
+
+        [TestMethod]
+        public void ThreeWithComment()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "UW.Name=george", "# Wat is going on today?", "UW.NoWay = YES");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.AreEqual(2, r["UW"].Count);
+        }
+
+        [TestMethod]
+        public void CommentsAndBlankLines()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "#", "# This is a sample file", "# ", "", "", "UW.Name=bogus #this is the first line", "", "UW.Focus =Yes");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.AreEqual(2, r["UW"].Count);
+        }
+
+        [TestMethod]
+        public void StartWithComment()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "#dude what are you doing?", "UW.Name=george", "# Wat is going on today?", "UW.NoWay = YES");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.AreEqual(2, r["UW"].Count);
+        }
+
+        [TestMethod]
+        public void EndWithComment()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "UW.Name=george # and no way this worked", "UW.NoWay = YES");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.AreEqual(2, r["UW"].Count);
+            Assert.AreEqual("george", r["UW"]["Name"]);
+        }
+
+        [TestMethod]
+        public void ThreeLines()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "UW.Name=george", "CERN.Day=50", "ND.Never=10");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(3, r.Count);
+        }
+
+        [TestMethod]
+        public void BlankLines()
+        {
+            var f = WriteConfigFile("SingleLineWithSpace3.txt", "UW.Name=george", "", "UW.NoWay=YES", "");
+            var r = Config.ParseConfigFile(f);
+            Assert.AreEqual(1, r.Count);
+            Assert.IsTrue(r.ContainsKey("UW"));
+            Assert.AreEqual(2, r["UW"].Count);
+        }
+
+        /// <summary>
+        /// Generate a config file
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        private FileInfo WriteConfigFile(string fname, params string[] p)
+        {
+            var f = new FileInfo(fname);
+            using (var wr = f.CreateText())
+            {
+                foreach (var l in p)
+                {
+                    wr.WriteLine(l);
+                }
+            }
+            return f;
+        }
+    }
+}
