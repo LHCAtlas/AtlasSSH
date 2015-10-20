f3eb0bce49d0fc529829b1ad8e16ee0e30e8d839
Gordon Watts <gordon@gordonandpaula.net>
2015-10-19 23:07:39 -0700

    Translates dataset names to output dataset names
    Fixes #56
    Fixes #53

---
+++ AtlasWorkFlows/Jobs/Datasets.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AtlasWorkFlows.Jobs
+{
+    /// <summary>
+    /// Deal with dataset functions
+    /// </summary>
+    static class Datasets
+    {
+        /// <summary>
+        /// Returns a dataset name that this job should produce given the original dataset name.
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="orignalDSName"></param>
+        /// <returns></returns>
+        public static string ResultingDatasetName (this Job job, string orignalDSName, string scopeDSName)
+        {
+            // Remove the scope if it is there.
+            var result = orignalDSName.RemoveBefore(":");
+
+            // Split the dataset into its consituant parts.
+            var dsParts = result.Split('.').ToList();
+
+            // If the first item in the list is a user, remove that and the next item.
+            if (dsParts[0] == "user")
+            {
+                dsParts.RemoveAt(0);
+                dsParts.RemoveAt(0);
+            }
+
+            // Remove a few key words
+            dsParts = dsParts
+                .Where(i => i != "merge")
+                .Where(i => i != "AOD")
+                .ToList();
+
+            // Look for the tid job spec. Remove it if it is there.
+            var containsTID = dsParts.Where(i => i.Contains("tid")).FirstOrDefault();
+            if (containsTID != null)
+            {
+                var minusTID = containsTID.RemoveTIDString();
+                dsParts.Replace(containsTID, minusTID);
+            }
+
+            // Ok, if the second guy is a number, then it is a run number. That means the first one is almost certianly a scope. So remove it.
+            int runNumber = 0;
+            if (dsParts.Count > 1 && int.TryParse(dsParts[1], out runNumber))
+            {
+                dsParts.RemoveAt(0);
+            }
+
+            // Done. Combine into a single string and check length!
+            var dsNew = scopeDSName + "." + dsParts.AsSingleString(".") + "." + job.Name + "_v" + job.Version + "_" + job.Hash().AsPositiveNumber();
+            while ((dsNew + "_hist-output.root/").Length > 133)
+            {
+                var oneLess = dsNew.RemoveATag();
+                if (oneLess == dsNew)
+                {
+                    StringBuilder bld = new StringBuilder();
+                    bld.AppendFormat("Dataset '{0}' is more than 133 characters after accounting for EventLoop naming (from ds '{1}'). It needs to be made shorter", dsNew, orignalDSName);
+                    throw new ArgumentException(bld.ToString());
+                }
+                dsNew = oneLess;
+            }
+
+            return dsNew;
+        }
+
+        /// <summary>
+        /// Remove everythign in the string before the search string. Return full string if the search string is not found.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="removeThisAndBefore"></param>
+        /// <returns></returns>
+        private static string RemoveBefore (this string src, string removeThisAndBefore)
+        {
+            var i = src.IndexOf(removeThisAndBefore);
+            if (i < 0)
+                return src;
+
+            return src.Substring(i + removeThisAndBefore.Length);
+        }
+
+        /// <summary>
+        /// Combine a list of strings into a single string with a seperator.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="seperator"></param>
+        /// <returns></returns>
+        private static string AsSingleString (this IEnumerable<string> items, string seperator)
+        {
+            var bld = new StringBuilder();
+            bool first = true;
+            foreach (var item in items)
+            {
+                if (!first)
+                {
+                    bld.Append(".");
+                }
+                bld.Append(item);
+                first = false;
+            }
+            return bld.ToString();
+        }
+
+        static Regex gTIDFinder = new Regex("_tid[0-9]+_[0-9]+");
+
+        /// <summary>
+        /// Remove a string that contains tid-nubmers-_-numbers-.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static string RemoveTIDString(this string input)
+        {
+            return gTIDFinder.Replace(input, "");
+        }
+
+        /// <summary>
+        /// Replace an element of a list. Not efficient!
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="oldValue"></param>
+        /// <param name="newValue"></param>
+        private static void Replace<T> (this List<T> items, T oldValue, T newValue)
+        {
+            var loc = items.IndexOf(oldValue);
+            if (loc > 0)
+            {
+                items[loc] = newValue;
+            }
+        }
+
+        static Regex gTagFinder = new Regex(@"[_\.][a-z][0-9]+[_\.]");
+
+        /// <summary>
+        /// Attempt to remove a reconstuction tag.
+        /// </summary>
+        /// <param name="dsName"></param>
+        /// <returns></returns>
+        private static string RemoveATag (this string dsName)
+        {
+            var m = gTagFinder.Match(dsName);
+            if (m.Success)
+            {
+                return dsName.Replace(m.Groups[0].Value, ".");
+            }
+            // No more tags to replace
+            return dsName;
+        }
+
+        /// <summary>
+        /// Make sure the number is postive.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private static int AsPositiveNumber(this int num)
+        {
+            if (num < 0)
+                return -num;
+            return num;
+        }
+    }
+}
---
+++ AtlasWorkFlowsTest/Jobs/DatasetTest.cs
@@ -0,0 +1,62 @@
+﻿using AtlasWorkFlows.Jobs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlasWorkFlowsTest.Jobs
+{
+    [TestClass]
+    public class DatasetTest
+    {
+        [TestMethod]
+        public void xAODMCDataset()
+        {
+            var j = new Job() { Name = "DiVertAnalysis", Version = 22 };
+            var ds = j.ResultingDatasetName("mc15_13TeV.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.merge.AOD.e3569_s2576_s2132_r6765_r6282", "user.bogus");
+            Assert.AreEqual("user.bogus.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.s2576_s2132_r6765_r6282.DiVertAnalysis_v22_1044055743", ds);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void xAODMCDatasetTooLong()
+        {
+            var j = new Job() { Name = "DiVertAnalysis", Version = 22 };
+            var ds = j.ResultingDatasetName("mc15_13TeV.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W_Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W_Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.merge.AOD.e3569_s2576_s2132_r6765_r6282", "user.bogus");
+        }
+
+        [TestMethod]
+        public void DxAODDatasetFromEmma()
+        {
+            var j = new Job() { Name = "DiVertAnalysis", Version = 22 };
+            var ds = j.ResultingDatasetName("user.emmat.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.AOD.e3668_s2576_s2132_r6765_r6282__EXOT15_EXT0", "user.bogus");
+            Assert.AreEqual("user.bogus.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.r6765_r6282__EXOT15_EXT0.DiVertAnalysis_v22_1044055743", ds);
+        }
+
+        [TestMethod]
+        public void LatestreamDS()
+        {
+            var j = new Job() { Name = "DiVertAnalysis", Version = 22 };
+            var ds = j.ResultingDatasetName("data15_13TeV:data15_13TeV.00277025.physics_Late.merge.DAOD_EXOT15.f622_m1486_p2425_tid06570038_00", "user.bogus");
+            Assert.AreEqual("user.bogus.00277025.physics_Late.DAOD_EXOT15.f622_m1486_p2425.DiVertAnalysis_v22_1044055743", ds);
+        }
+
+        [TestMethod]
+        public void LatestreamDSNoScope()
+        {
+            var j = new Job() { Name = "DiVertAnalysis", Version = 22 };
+            var ds = j.ResultingDatasetName("data15_13TeV.00277025.physics_Late.merge.DAOD_EXOT15.f622_m1486_p2425_tid06570038_00", "user.bogus");
+            Assert.AreEqual("user.bogus.00277025.physics_Late.DAOD_EXOT15.f622_m1486_p2425.DiVertAnalysis_v22_1044055743", ds);
+        }
+
+        [TestMethod]
+        public void DataDerivationxAOD()
+        {
+            var j = new Job() { Name = "DiVertAnalysis", Version = 22 };
+            var ds = j.ResultingDatasetName("data15_13TeV:data15_13TeV.00280500.physics_Main.merge.DAOD_EXOT15.f631_m1504_p2425_tid06603342_00", "user.bogus");
+            Assert.AreEqual("user.bogus.00280500.physics_Main.DAOD_EXOT15.f631_m1504_p2425.DiVertAnalysis_v22_1044055743", ds);
+        }
+    }
+}
