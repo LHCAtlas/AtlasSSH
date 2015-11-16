c871c64bf051b2a6a35077e2667eb0fe98e15d53
Gordon Watts <gordon@gordonandpaula.net>
2015-11-15 17:55:21 -0800

    Fix up broken tests
    #94

--- AtlasWorkFlowsTest/Jobs/DatasetTest.cs
+++ AtlasWorkFlowsTest/Jobs/DatasetTest.cs
@@ -16,7 +16,7 @@ namespace AtlasWorkFlowsTest.Jobs
         {
             var j = new AtlasJob() { Name = "DiVertAnalysis", Version = 22 };
             var ds = j.ResultingDatasetName("mc15_13TeV.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.merge.AOD.e3569_s2576_s2132_r6765_r6282", "user.bogus");
-            Assert.AreEqual("user.bogus.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.s2576_s2132_r6765_r6282.DiVertAnalysis_v22_1044055743", ds);
+            Assert.AreEqual("user.bogus.361020.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ0W.e3569_s2576_s2132_r6765_r6282.DiVertAnalysis_v22_3B233454", ds);
         }
 
         [TestMethod]
@@ -32,7 +32,7 @@ namespace AtlasWorkFlowsTest.Jobs
         {
             var j = new AtlasJob() { Name = "DiVertAnalysis", Version = 22 };
             var ds = j.ResultingDatasetName("user.emmat.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.merge.AOD.e3668_s2576_s2132_r6765_r6282__EXOT15_EXT0", "user.bogus");
-            Assert.AreEqual("user.bogus.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.r6765_r6282__EXOT15_EXT0.DiVertAnalysis_v22_1044055743", ds);
+            Assert.AreEqual("user.bogus.361023.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ3W.s2132_r6765_r6282__EXOT15_EXT0.DiVertAnalysis_v22_3B233454", ds);
         }
 
         [TestMethod]
@@ -40,7 +40,7 @@ namespace AtlasWorkFlowsTest.Jobs
         {
             var j = new AtlasJob() { Name = "DiVertAnalysis", Version = 22 };
             var ds = j.ResultingDatasetName("data15_13TeV:data15_13TeV.00277025.physics_Late.merge.DAOD_EXOT15.f622_m1486_p2425_tid06570038_00", "user.bogus");
-            Assert.AreEqual("user.bogus.00277025.physics_Late.DAOD_EXOT15.f622_m1486_p2425.DiVertAnalysis_v22_1044055743", ds);
+            Assert.AreEqual("user.bogus.00277025.physics_Late.DAOD_EXOT15.f622_m1486_p2425.DiVertAnalysis_v22_3B233454", ds);
         }
 
         [TestMethod]
@@ -48,7 +48,7 @@ namespace AtlasWorkFlowsTest.Jobs
         {
             var j = new AtlasJob() { Name = "DiVertAnalysis", Version = 22 };
             var ds = j.ResultingDatasetName("data15_13TeV.00277025.physics_Late.merge.DAOD_EXOT15.f622_m1486_p2425_tid06570038_00", "user.bogus");
-            Assert.AreEqual("user.bogus.00277025.physics_Late.DAOD_EXOT15.f622_m1486_p2425.DiVertAnalysis_v22_1044055743", ds);
+            Assert.AreEqual("user.bogus.00277025.physics_Late.DAOD_EXOT15.f622_m1486_p2425.DiVertAnalysis_v22_3B233454", ds);
         }
 
         [TestMethod]
@@ -56,7 +56,7 @@ namespace AtlasWorkFlowsTest.Jobs
         {
             var j = new AtlasJob() { Name = "DiVertAnalysis", Version = 22 };
             var ds = j.ResultingDatasetName("data15_13TeV:data15_13TeV.00280500.physics_Main.merge.DAOD_EXOT15.f631_m1504_p2425_tid06603342_00", "user.bogus");
-            Assert.AreEqual("user.bogus.00280500.physics_Main.DAOD_EXOT15.f631_m1504_p2425.DiVertAnalysis_v22_1044055743", ds);
+            Assert.AreEqual("user.bogus.00280500.physics_Main.DAOD_EXOT15.f631_m1504_p2425.DiVertAnalysis_v22_3B233454", ds);
         }
 
         [TestMethod]
