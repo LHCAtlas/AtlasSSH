5d8f51321ef62885201800fcdfe6da74e6c43df5
Gordon Watts <gordon@gordonandpaula.net>
2016-06-26 14:25:28 -0700

    Fix up a comment.

--- AtlasWorkFlows/Locations/GRIDFetchToLinuxVisibleOnWindows.cs
+++ AtlasWorkFlows/Locations/GRIDFetchToLinuxVisibleOnWindows.cs
@@ -10,7 +10,7 @@ namespace AtlasWorkFlows.Locations
 {
     /// <summary>
     /// Holds code to enable a download of a dataset to a location on a grid connected node on Linux
-    /// and transfer that code to a place that is visible on Windows (via a NAS or x-mount).
+    /// and transfer that data to a place that is visible on Windows (via a NAS or x-mount).
     /// </summary>
     class GRIDFetchToLinuxVisibleOnWindows
     {
