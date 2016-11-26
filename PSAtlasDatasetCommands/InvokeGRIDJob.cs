71fa3200cde4e3749bef5f53cc4c9df50306a313
Gordon Watts <gordon@gordonandpaula.net>
2016-11-26 01:48:05 -0800

    Sort out usings

--- PSAtlasDatasetCommands/InvokeGRIDJob.cs
+++ PSAtlasDatasetCommands/InvokeGRIDJob.cs
@@ -5,12 +5,9 @@ using CredentialManagement;
 using Polly;
 using PSAtlasDatasetCommands.Utils;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Management.Automation;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PSAtlasDatasetCommands
 {
