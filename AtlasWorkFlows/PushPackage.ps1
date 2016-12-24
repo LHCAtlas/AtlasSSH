395686ff71b25f3f33f1b80b58a6b9255391ebbb
Gordon Watts <gordon@gordonandpaula.net>
2016-12-23 23:17:01 -0500

    Get it so we are now pushign the library to nuget as well.

---
+++ AtlasWorkFlows/PushPackage.ps1
@@ -0,0 +1,20 @@
+﻿# Push the package if the version number has changed.
+#
+[CmdletBinding()]
+param()
+
+# Get the current package numbers
+$pkgName = "AtlasSSH"
+$current = $(nuget list $pkgName)
+$version = $current.Split()[1]
+Write-Verbose "Nuget has version $version of Package $pkgName"
+
+# Get the current contents of the nuspec file
+$nuspec = Get-Content .\AtlasWorkFlows.nuspec | Select-String $version
+if ($nuspec.Count -gt 0) {
+	Write-Verbose "Version $version is already on the myget server. Will not push."
+} else {
+    rm .\*.nupkg
+	nuget pack .\AtlasWorkFlows.nuspec
+	nuget push .\AtlasWorkFlows.*.nupkg
+}
