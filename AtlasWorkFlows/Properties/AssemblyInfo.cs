afc8b644c7535a7e0c25a411a5fe56805334978c
Gordon Watts <gordon@gordonandpaula.net>
2016-12-30 01:12:56 -0500

    Update to first edition of 2.7

--- AtlasWorkFlows/Properties/AssemblyInfo.cs
+++ AtlasWorkFlows/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gordon Watts")]
 [assembly: AssemblyProduct("AtlasWorkFlows")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.4.1")]
-[assembly: AssemblyFileVersion("2.6.4.1")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
 
 // Allow for easier testing
 [assembly: InternalsVisibleTo("AtlasWorkFlowsTest")]
