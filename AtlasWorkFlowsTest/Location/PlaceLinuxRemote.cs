d6ef480b808a795ec1e20d402dc06952715f1eda
Gordon Watts <gordon@gordonandpaula.net>
2017-01-03 01:52:29 -0800

    Skeleton for the linux end point #142

---
+++ AtlasWorkFlowsTest/Location/PlaceLinuxRemote.cs
@@ -0,0 +1,104 @@
+﻿using AtlasWorkFlows.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlasWorkFlowsTest.Location
+{
+    /// <summary>
+    /// This is a remote linux machine.
+    /// - Files are accessible only as a repository - copy in or out
+    /// - If the disks are availible on SAMBA, then create a WindowsLocal as well
+    /// - If GRID can download here, then that should also create a new place.
+    /// </summary>
+    class PlaceLinuxRemote : IPlace
+    {
+        public PlaceLinuxRemote(string name)
+        {
+            DataTier = 50;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Get/Set the data tier. We default to 50 as we have no local access.
+        /// </summary>
+        public int DataTier { get; set; }
+
+        /// <summary>
+        /// We do not implement any local access from this end point
+        /// </summary>
+        public bool IsLocal { get { return false; } }
+
+        /// <summary>
+        /// Get the name of this repro.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Does not need confirmation to use.
+        /// </summary>
+        public bool NeedsConfirmationCopy { get { return false; } }
+
+        /// <summary>
+        /// We can start a copy from here to other places that have a SSH destination availible.
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public bool CanSourceCopy(IPlace destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Start a copy from the <paramref name="origin"/> via SSH.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="uris"></param>
+        public void CopyFrom(IPlace origin, Uri[] uris)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Start a copy pushing data to a particular destination.
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <param name="uris"></param>
+        public void CopyTo(IPlace destination, Uri[] uris)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Look at our directory structure to get a listing of files for a particular dataset.
+        /// </summary>
+        /// <param name="dsname"></param>
+        /// <returns></returns>
+        public string[] GetListOfFilesForDataset(string dsname)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Since we aren't local, we never return file locations. Always throw.
+        /// </summary>
+        /// <param name="uris"></param>
+        /// <returns></returns>
+        public IEnumerable<Uri> GetLocalFileLocations(IEnumerable<Uri> uris)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// See if we have a particular file in our dataset
+        /// </summary>
+        /// <param name="u"></param>
+        /// <returns></returns>
+        public bool HasFile(Uri u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
